08c06320aa0be667d897684528fb9ff7c9e3ac8c
INGENIOUS <sagarabhyankar18@gmail.com>
2023-05-01 11:19:44 +0530

    adminAuth

--- middleware/auth.js
+++ middleware/auth.js
@@ -20,5 +20,31 @@ const auth = async (req, res, next) => {
     }
   };
 
+const authAdmin = async (req, res, next) => { 
+    try {
+        const authHeader = req.headers.authorization; //send in header!!
+        if (authHeader) {
+            const token = authHeader.split(' ')[1];
+            const verifiedUser = jwt.verify(token, process.env.SECRET_KEY);
+            const user = await User.findOne({_id: verifiedUser._id});
+            if(user.privilege_level == 0){
+                req.user = user;
+                next();
+            }
+            else{
+                res.status(401).send({message: "Unauthorized"});
+            }
+        } else {
+            res.status(401).send({message: "Missing Authorization header"});
+        }
+    } catch(err) {
+
+        console.error(err);
+        res.status(401).send({message: "Invalid Authorization header"});
+    }
+};
+
+
+
 module.exports = auth;
 
